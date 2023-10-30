1d6e088cfe90ffeffb6916f94a26527e6f45d35e
Lucas <lucas.araruna07@gmail.com>
2023-10-30 16:26:40 -0300

    text-color-changing

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -44,7 +44,7 @@ export function Widget() {
                     <div className="bg-zinc-900 px-8 py-4 flex items-start gap-6">
                         <Rocket className='w-6 h-6 text-violet-500 mt-3' />
                         <div className='flex-1 flex flex-col gap-1'>
-                            <p className='text-sm leading-relaxed text-zinc-100'>Você recebeu um convite para fazer parte da empresa Rocketseat.</p>
+                            <p className='text-sm leading-relaxed text-zinc-400'>Você recebeu um convite para fazer parte da empresa Rocketseat.</p>
                             <div className='text-xxs text-zinc-400 flex items-center gap-1'>
                                 <span>Convite</span>
                                 <span>Há 3 min</span>
@@ -55,7 +55,7 @@ export function Widget() {
                     <div className="bg-zinc-900 px-8 py-4 flex items-start gap-6">
                         <Rocket className='w-6 h-6 text-violet-500 mt-3' />
                         <div className='flex-1 flex flex-col gap-1'>
-                            <p className='text-sm leading-relaxed text-zinc-100'>Você recebeu um convite para fazer parte da empresa Rocketseat.</p>
+                            <p className='text-sm leading-relaxed text-zinc-400'>Você recebeu um convite para fazer parte da empresa Rocketseat.</p>
                             <div className='text-xxs text-zinc-400 flex items-center gap-1'>
                                 <span>Convite</span>
                                 <span>Há 3 min</span>
